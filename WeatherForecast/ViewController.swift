bfba85f3d80f79eec5e84d8d3750450bb32a97b9
Christopher Batin <christopher.batin@skybettingandgaming.com>
2018-04-13 14:51:56 +0100

    Init Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Batin, Christopher on 13/04/2018.
+//  Copyright © 2018 Batin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
